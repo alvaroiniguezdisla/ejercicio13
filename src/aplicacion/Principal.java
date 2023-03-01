3b41d4bb0f7da8876236bebac728748eafa86322
Álvaro <ainiguezdislaa@gmail.com>
2023-03-01 09:07:15 +0100

    ej13 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,34 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        n = sc.nextInt();
+
+        System.out.println("Suma de los  números hasta " + n +" :"+ dominio.ej13.suma(n));
+        System.out.println("Factorial de  " + n+" :"+ dominio.ej13.factorial(n));
+        System.out.println("Potencia de 2 elevado a 10: " + dominio.ej13.potencia(2, 10));
+        System.out.println("Suma de los elementos de la lista: " + dominio.ej13.sumalista(lista));
+        System.out.println("Media de los elementos de la lista: " + dominio.ej13.media(lista));
+        System.out.println("Desviación " + dominio.ej13.desviacion(lista));
+        System.out.println("Suma de los elementos hasta  " +n+" :"+ dominio.ej13.sumapares(n));
+        System.out.println("Suma de los elementos pares de la lista: " + dominio.ej13.sumapares(n));
+        System.out.println("Lista de los elementos pares de la lista: " + dominio.ej13.nuevalistapares(lista));
+        System.out.println("Lista con los elementos pares hasta "+n+" :" + dominio.ej13.listapares(n));
+        System.out.println("Producto escalar de dos listas: " + dominio.ej13.productoescalar(lista, lista));
+        System.out.println("Fibonacci de  " + n +" :"+ dominio.ej13.fibonacci(n));
+        System.out.println("La relación entre los resultados de la razón aurea y fibonacci es "+dominio.ej13.comparar((1+Math.sqrt(5))/2));
+
+
+
+
+
+
+
+
+
+    }
 }
--- src/dominio/ej13.java
+++ src/dominio/ej13.java
@@ -1,2 +1,78 @@
-package dominio;public class ej13 {
+package dominio;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ej13 {
+    public static int suma(int n) {
+        return Stream.iterate(0, i -> i + 1).limit(n+1).reduce(0, (a, b) -> a + b);
+    }
+    public static int factorial(int n){
+        return Stream.iterate(1,i->i+1).limit(n).reduce(1,(a,b)->a*b);
+    }
+    public static int potencia (int base, int exponente) {
+        return Stream.iterate(1, i -> i+1).limit(exponente).reduce(1,(a,b)->a*base);
+    }
+    public static int sumalista(int[]lista){
+        return Stream.iterate(0, i -> i + 1).limit(lista.length).map(i -> lista[i]).reduce(0, (a, b) -> a + b);
+    }
+
+
+    public static int media(int[]lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b)/lista.length;
+    }
+    public static int cuadrados(int[]lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b*b);
+    }
+    public static double desviacion(int [] lista){
+        double media=media(lista);
+        double cuadrados=cuadrados(lista);
+        return Math.sqrt((cuadrados/lista.length)-(media*media));
+    }
+
+    public static int sumapares(int n){
+        return Stream.iterate(0,i->i+1).limit(n+1).filter(i->i%2==0).reduce(0,(a,b)->a+b);
+    }
+
+    public static int sumalistapares (int[] lista){
+        return  Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).filter(i->i%2==0).reduce(0,(a,b)->a+b);
+    }
+
+    public static ArrayList nuevalistapares(int[] lista){
+        ArrayList listanueva = new ArrayList();
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).filter(i->i%2==0).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static ArrayList listapares(int n){
+        return Stream.iterate(0,i->i+1).limit(n).filter(i->i%2==0).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static int productoescalar(int[]lista1,int[]lista2){
+        return Stream.iterate(0,i->i+1).limit(lista1.length).map(i->lista1[i]*lista2[i]).reduce(0,(a,b)->a+b);
+    }
+
+    public static int fibonacci(int n){
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]}).limit(n).map(f -> f[0]).reduce(0, (a, b) -> b);
+    }
+
+    public static int fib13() {
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]}).limit(13).map(f -> f[0]).reduce(0, (a, b) ->  b);
+    }
+
+    public static int fib14() {
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]}).limit(14).map(f -> f[0]).reduce(0, (a, b) -> b);
+    }
+
+    public static boolean comparar(double a) {
+        double fib = (double)fib14() / fib13();
+        if (Math.abs(a - fib) < 0.1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+
 }
